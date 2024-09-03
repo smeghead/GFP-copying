1b1782a242e5188cbc2ecee007395c987a5dd16c
smeghead <smeghead7@gmail.com>
2024-09-03 21:15:28 +0900

    implove

--- src/main/scala/ch03_15.scala
+++ src/main/scala/ch03_15.scala
@@ -1,11 +1,7 @@
 object Ch02_15 {
   def abbreviate(str: String): String = {
-    val parts = str.split(" ")
-    val firstName = parts(0)
-    val lastName = parts(1)
-    val abbr =
-      if (firstName.length == 1) firstName
-      else firstName.substring(0, 1)
-    return String.format("%s. %s", abbr, lastName)
+    val separaterIndex = str.indexOf(" ")
+    val firstName = str.substring(0, separaterIndex)
+    return String.format("%s. %s", firstName.substring(0, 1), str.substring(separaterIndex + 1))
   }
 }
