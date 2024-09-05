d1d853ee0bcdae86fb6e6ab8eeac085946c1f492
smeghead <smeghead7@gmail.com>
2024-09-05 23:01:46 +0900

    remove comment.

--- src/main/scala/ch04_50.scala
+++ src/main/scala/ch04_50.scala
@@ -38,31 +38,5 @@ object Ch04_50 {
       assert(f(List("scala", "ada")) == List.empty)
     }
 
-//    val list2 = List(5, 1, 2, 4, 15)
-//    val result2: Int => List[Int] = div => list2.filter(n => n % div == 0)
-//
-//    println(result2(5))
-//    assert(result2(5) == List(5, 15))
-//
-//    println(result2(2))
-//    assert(result2(2) == List(2, 4))
-//
-//    val list3 = List("scala", "ada")
-//    val result3: Int => List[String] = len => list3.filter(word => word.length < len)
-//
-//    println(result3(5))
-//    assert(result3(5) == List("ada"))
-//
-//    println(result3(7))
-//    assert(result3(7) == List("scala", "ada"))
-//
-//    val list4 = List("rust", "ada")
-//    val result4: Int => List[String] = count => list4.filter(word => word.filter(w => w == 's').length >= count)
-//
-//    println(result4(3))
-//    assert(result4(3) == List.empty)
-//
-//    println(result4(1))
-//    assert(result4(1) == List("rust"))
   }
 }
