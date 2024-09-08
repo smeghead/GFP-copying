27143a4b800d4b66450eb69aa74a15ab638c86b6
smeghead <smeghead7@gmail.com>
2024-09-08 21:22:05 +0900

    playlist modeling.

--- src/main/scala/ch07_09.scala
+++ src/main/scala/ch07_09.scala
@@ -8,8 +8,6 @@ object Ch07_09 {
   }
   import MusicGenre._
 
-  case class Location(name: String)
-
   enum YearsActive {
     case StillActive(since: Int)
     case ActiveBetween(start: Int, end: Int)
@@ -23,12 +21,11 @@ object Ch07_09 {
     }
   }
 
-  // // newtype がエラーになる。
-  // opaque type Location = String
-  // object Location {
-  //   def apply(value: String): Location = value
-  //   extension(a: Location) def name: String = a
-  // }
+  opaque type Location = String
+  object Location {
+    def apply(value: String): Location = value
+    extension(a: Location) def name: String = a
+  }
 
   case class Artist(
     name: String,
@@ -45,17 +42,17 @@ object Ch07_09 {
     activeAfter: Int,
     activeBefore: Int
   ): List[Artist] = {
-    val genreResult = if (genres.length > 0)
-                        artists.filter(artist => genres.contains(artist.genre))
-                      else artists
-    val locationResult = if (locations.length > 0)
-                           genreResult.filter(artist => locations.contains(artist.origin.name))
-                         else genreResult
-    if (searchByActiveYears)
-      locationResult.filter(artist => wasArtistActive(artist, activeAfter, activeBefore))
-      //  artist.yearsActive.start <= activeBefore &&
-      //                                (artist.yearsActive.end.forall(_ >= activeAfter))
-    else locationResult
+    artists.filter(artist =>
+      (genres.isEmpty || genres.contains(artist.genre)) &&
+      (locations.isEmpty || locations.contains(artist.origin)) &&
+      (searchByActiveYears || wasArtistActive(artist, activeAfter, activeBefore)))
+  }
+
+  def activeLength(artist: Artist, currentYear: Int): Int = {
+    artist.yearsActive match {
+      case StillActive(since) => currentYear - since
+      case ActiveBetween(start, end) => end - start
+    }
   }
 
   val artists = List(
@@ -85,6 +82,30 @@ object Ch07_09 {
       val result = searchArtists(artists, List.empty, List.empty, false, 2019, 2022)
       println(result)
     }
-
+    println("7.29 Pattern Matching")
+    {
+      val result = activeLength(Artist(
+        "Metallica",
+        HeavyMetal,
+        Location("U.S."),
+        StillActive(1981)), 2022)
+      println(result)
+      assert(result == 41)
+    }
+    {
+      val result = activeLength(Artist(
+        "Led Zeppelin",
+        HardRock,
+        Location("England"),
+        ActiveBetween(1968, 1980)), 2022)
+      println(result)
+      assert(result == 12)
+    }
+    {
+      val result = activeLength(Artist(
+        "Bee Gees", Pop, Location("England"), ActiveBetween(1958, 2003)), 2022)
+      println(result)
+      assert(result == 45)
+    }
   }
 }
---
+++ src/main/scala/ch07_32.scala
@@ -0,0 +1,82 @@
+
+object Ch07_32 {
+
+  enum MusicGenre {
+    case Funk
+    case House
+    case Rock
+  }
+  import MusicGenre._
+
+  opaque type Artist = String
+  object Artist {
+    def apply(value: String): Artist = value
+    extension(a: Artist) def name: String = a
+  }
+
+  opaque type User = String
+  object User {
+    def apply(value: String): User = value
+    extension(a: User) def name: String = a
+  }
+
+  enum PlaylistType {
+    case CustomSongsList(name: User)
+    case ArtistSongsList(artist: Artist)
+    case GenreSongsList(genres: List[MusicGenre])
+  }
+  import PlaylistType._
+
+  case class Song(name: String, artist: Artist)
+
+  case class Playlist(name: String, playlistType: PlaylistType, songs: List[Song])
+
+  def gatherSongs(playlists: List[Playlist], artist: Artist, genre: MusicGenre): List[Song] = {
+    playlists.flatMap(playlist =>
+      playlist.playlistType match {
+        case CustomSongsList(name) => playlist.songs
+        case ArtistSongsList(a) => if (a == artist) playlist.songs else List.empty
+        case GenreSongsList(genres) => if (genres.contains(genre)) playlist.songs else List.empty
+      }
+    )
+  }
+
+  val playlists = List(
+    Playlist(
+      "This is Foo Fighters",
+      ArtistSongsList(Artist("Foo Fighters")),
+      List(
+        Song("Breakout", Artist("Foo Fighters")),
+        Song("Learn To Fly", Artist("Foo Fighters"))
+      )),
+    Playlist(
+      "Deep Focus",
+      GenreSongsList(List(Funk, House)),
+      List(
+        Song("One More Time", Artist("Daft Punk")),
+        Song("Hey Boy Hey Girl", Artist("Chemical Brothers"))
+      )
+    ),
+    Playlist(
+      "User's",
+      CustomSongsList(User("me")),
+      List(
+        Song("Hello, good by", Artist("The Beatles")),
+        Song("Jumping Jack Flash", Artist("The Rolling Stones"))
+      )
+    )
+  )
+
+  def run(): Unit = {
+    {
+      val result = gatherSongs(playlists, Artist("Foo Fighters"), Rock)
+      println("Foo Fighters, Rock")
+      println(result)
+    }
+    {
+      val result = gatherSongs(playlists, Artist("Who"), Funk)
+      println("Who, Funk")
+      println(result)
+    }
+  }
+}
