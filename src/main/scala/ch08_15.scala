705b24654d071170d8cd2da442e843bfcf944e82
smeghead <smeghead7@gmail.com>
2024-09-10 00:16:01 +0900

    schedule function.

--- src/main/scala/ch08_15.scala
+++ src/main/scala/ch08_15.scala
@@ -1,13 +1,13 @@
 
 object Ch08_15 {
 
-  case class MeetingTime (name: String, start: Int, end: Int)
+  case class MeetingTime (start: Int, end: Int)
 
 
   def calendarEntriesApiCall(name: String): List[MeetingTime] = {
     println("start calendarEntriesApiCall")
 
-    List(MeetingTime("Alice", 8, 9))
+    List(MeetingTime(8, 9))
   }
 
   def calendarEntries(name: String): IO[List[MeetingTime]] = {
@@ -27,6 +27,17 @@ object Ch08_15 {
       person1meetings <- calendarEntries(person1)
       person2meetings <- calendarEntries(person2)
     } yield person1meetings.appendedAll(person2meetings)
+  }
+
+  def firstSpace(meetings: List[MeetingTime], lengthHours: Int): Option[MeetingTime] = {
+    val spaces = (8 to 16 - lengthHours).toList.map(n => MeetingTime(n, n + lengthHours))
+    spaces.find(s => meetings.exists(m => s.start > m.end || s.end < m.start))
+  }
+
+  def schedule(person1: String, person2: String, lengthHours: Int): IO[Option[MeetingTime]] = {
+    for {
+      mettings <- scheduledMeetings(person1, person2)
+    } yield firstSpace(meetings, lengthHours)
   }
 
   def run(): Unit = {
